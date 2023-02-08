04b5aebc5eeb1a4a20143e22844a41dca46dbfb4
Cynthia Guzman <cynthiaguzman@Cynthias-MacBook-Air-2.local>
2023-02-07 23:24:45 -0600

    tried to style as much as I could and added README file

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var weatherHisDiv = document.getElementById('weatherHis');
 
     cityArray.forEach(function(city){
       var button = document.createElement("button");
-      button.style.backgroundColor='purple';
+      button.style.backgroundColor='lightblue';
       document.body.appendChild(button);
       button.innerText = city;
       button.addEventListener("click",() => {fetchweather(city)});
@@ -33,43 +33,43 @@ function fetchweather(city) {
 
   var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + myApiKey;
 
-  fetch(weatherUrl)
-      .then(function (response) {
-          if (!response.ok) {
-              throw response.json();
-          }
-          return response.json();
-      })
-      .then(function (weatherRes) {
+   fetch(weatherUrl)
+       .then(function (response) {
+           if (!response.ok) {
+               throw response.json();
+           }
+           return response.json();
+       })
+       .then(function (weatherRes) {
           var weatherArray = weatherRes.list;
 
-          var oldDate = weatherArray[0].dt_txt.split(" ")[0];
-          var newDate;
-          var weatherHTML = "<table><tr><td colspan='5'>";
-          weatherHTML += "<table border='1'>";
-          weatherHTML += "<tr><td>" + city + " (" + oldDate + ")<img src='http://openweathermap.org/img/wn/" + weatherArray[0].weather[0].icon + ".png'></img></td></tr>";
+           var oldDate = weatherArray[0].dt_txt.split(" ")[0];
+           var newDate;
+          var weatherHTML= "<table <tr><td colspan='5'>";
+           weatherHTML += "<table border='10' >";          
+           weatherHTML += "<tr><td>" + city + " (" + oldDate + ")<img src='http://openweathermap.org/img/wn/" + weatherArray[0].weather[0].icon + ".png'></img></td></tr>";
           weatherHTML += "<tr><td>Temp: " + weatherArray[0].main.temp + "&#176;F</td></tr>";
-          weatherHTML += "<tr><td>Wind: " + weatherArray[0].wind.speed + " MPH </td></tr>";
-          weatherHTML += "<tr><td>Humidity: " + weatherArray[0].main.humidity + "%</td></tr>";
+           weatherHTML += "<tr><td>Wind: " + weatherArray[0].wind.speed + " MPH </td></tr>";
+           weatherHTML += "<tr><td>Humidity: " + weatherArray[0].main.humidity + "%</td></tr>";
           weatherHTML += "</table>";
-          weatherHTML += "</td></tr><tr><td colspan='5'>5-Day Forecast:</td></tr><tr>";
+           weatherHTML += "</td></tr><tr><td colspan='5'>5-Day Forecast:</td></tr><tr>";
 
-          for (let i = 0; i < weatherArray.length; i++) {
+           for (let i = 0; i < weatherArray.length; i++) {
 
-              newDate = weatherArray[i].dt_txt.split(" ")[0];
+               newDate = weatherArray[i].dt_txt.split(" ")[0];
 
-              if (oldDate != newDate) {
-                  weatherHTML += "<td><table border='1'>";
+          if (oldDate != newDate) {
+             weatherHTML += "<td><table border='1'>";
                   weatherHTML += "<tr><td>" + newDate + "</td></tr>";
                   weatherHTML += "<tr><td><img src='http://openweathermap.org/img/wn/" + weatherArray[i].weather[0].icon + ".png'></img></td></tr>";
                   weatherHTML += "<tr><td>Temp: " + weatherArray[i].main.temp + "&#176;F</td></tr>";
                   weatherHTML += "<tr><td>Wind: " + weatherArray[i].wind.speed + " MPH </td></tr>";
-                  weatherHTML += "<tr><td>Humidity: " + weatherArray[i].main.humidity + "%</td></tr>";
-                  weatherHTML += "</table></td>";
+                   weatherHTML += "<tr><td>Humidity: " + weatherArray[i].main.humidity + "%</td></tr>";
+                   weatherHTML += "</table></td>";
 
                   oldDate = newDate;
-              }
-          }
-document.getElementById("weatherForecast").innerHTML = weatherHTML;
-        })}
-        
+               }
+           }
+ document.getElementById("weatherForecast").innerHTML = weatherHTML;
+})}
+
